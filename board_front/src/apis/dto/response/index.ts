24a4eedf3bb5c35ecfb4612c3ca8ec9b4abb5d5d
chang0506 <j8970@naver.com>
2025-05-20 11:44:48 +0900

    second

---
+++ board_front/src/apis/dto/response/index.ts
@@ -0,0 +1,7 @@
+import ResponseDto from "./responseDto";
+
+export type {
+  ResponseDto
+};
+
+// import { ResponseDto } from '@/apis/dto/response/';
---
+++ board_front/src/apis/dto/response/responseDto.ts
@@ -0,0 +1,6 @@
+// 공통 응답 타입: ResponseDto // 
+
+export default interface ResponDto {
+  result: boolean;
+  message: string;
+}
